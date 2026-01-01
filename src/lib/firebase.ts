ca3ab5f7247a9c54d439eb6986a870883df882ad
hklee03 <hklee03@cafe24corp.com>
2026-01-01 10:04:47 +0900

    feat: add Firebase configuration (shared with kid-chat)

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, FirebaseApp } from 'firebase/app';
+import { getAuth, Auth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxtXl0mnUpaMhkTPhlbYVr9o49oX7sJks",
+  authDomain: "kid-chat-2ca0f.firebaseapp.com",
+  projectId: "kid-chat-2ca0f",
+  storageBucket: "kid-chat-2ca0f.firebasestorage.app",
+  messagingSenderId: "1097422182914",
+  appId: "1:1097422182914:web:cb798a95684b0b0c39e51b",
+  measurementId: "G-YSWET2DE8E"
+};
+
+export const app: FirebaseApp = initializeApp(firebaseConfig);
+export const auth: Auth = getAuth(app);
